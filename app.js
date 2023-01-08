19bf4569518ff0c4fafdbc8833326285e2179744
Patrick Carter <patrickcarter@Patricks-Laptop.local>
2023-01-08 15:06:00 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const https = require('https');
+const { url } = require('inspector');
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+
+    res.sendFile(`${__dirname}/signup.html`);
+
+})
+
+app.post('/', (req, res) => {
+
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+
+    const data = {
+        members: [{
+            email_address: email,
+            status: 'subscribed',
+            merge_fields: {
+                FNAME: firstName,
+                LNAME: lastName
+            }
+        }]
+    };
+
+    const jsonData = JSON.stringify(data);
+    const url = 'https://us17.api.mailchimp.com/3.0/lists/1aef6f2294';
+    const options = {
+        method: 'POST',
+        auth: 'patrick1:d25ee497bdf9e1f90470594154b6e404-us17'
+    }
+
+    const request = https.request(url, options, (response) => {
+
+        if (response.statusCode === 200) {
+            res.sendFile(`${__dirname}/success.html`)
+        } else {
+            res.sendFile(`${__dirname}/failure.html`)
+        }
+
+        response.on('data', (data) => {
+            console.log(JSON.parse(data));
+        })
+    })
+
+    request.write(jsonData);
+    request.end();
+})
+
+app.post('/failure', (req, res) => {
+    res.redirect('/');
+})
+
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+
+// API Key
+// d25ee497bdf9e1f90470594154b6e404-us17
+
+// List ID
+// 1aef6f2294
